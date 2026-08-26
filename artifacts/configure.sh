1542e0d34101815d549e6ddab1aed593890538d7
Dharanidharan-S-04 <dean12345.dd@gmail.com>
2026-08-26 10:45:42 +0530

    Update configure.sh
    Signed-off-by: Dharanidharan-S-04 <dean12345.dd@gmail.com>

--- artifacts/configure.sh
+++ artifacts/configure.sh
@@ -84,4 +84,7 @@ echo master-templates zip creation started
 zip -r -j ${master_template_path}/master-templates.zip ${work_dir}/master-templates/*
 echo master-templates zip creation completed
 
-
+echo registration-api-impl zip creation started
+zip -r -j ${registration_api_impl_zip_path}/registration-api-impl.zip ${registration_api_impl_zip_path}/registration-api-impl/*
+rm -rf ${registration_api_impl_zip_path}/registration-api-impl
+echo registration-api-impl zip creation completed
